102de90676aa5a3341cba0d45d59e5a3984d70ca
yuxcer <yuxcer@126.com>
2012-11-14 15:42:08 +0800

    fix region.py 07

--- manifests/init.pp
+++ manifests/init.pp
@@ -35,7 +35,8 @@ class dough(
 	$admin_token,
 # region.py.example.erb setting
 	$default_region			= 'http://127.0.0.1:35357/v2.0/',
-	$dough_hash				= ['"RegionOne":"http://127.0.0.1:35357/v2.0/"'],
+	$region_name			= "RegionOne",
+	$region_endpoint		= "http://127.0.0.1:35357/v2.0/"
 ){
   include 'concat::setup'
 
